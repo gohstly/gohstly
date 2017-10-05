de00848846df9965cd33af27b11699417037b3ee
Garrett Moore <garrettmarkmoore@gmail.com>
2017-10-05 00:16:36 -0700

    Add Tabs to home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import PersistentDrawer from "../../components/Drawer";
+import TabsMain from "../../components/Tabs";
 
 
 class Home extends Component {
@@ -13,8 +14,10 @@ class Home extends Component {
     
       render() {
         return (
-          
+          <div>
             <PersistentDrawer/>
+            <TabsMain/>
+              </div>
             
          
         );
