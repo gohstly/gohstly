c061f37f42e83b693c2258c7efaa7872069d2551
Garrett Moore <garrettmarkmoore@gmail.com>
2017-10-06 00:51:59 -0700

    updated API to https

--- client/src/utils/steamAPI.js
+++ client/src/utils/steamAPI.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const FRIENDURL = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamid=";
-const RECENTURL = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamid=";
-const ALLURL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamid="
-const PROFILEURL = " http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamids="
+const FRIENDURL = "https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamid=";
+const RECENTURL = "https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamid=";
+const ALLURL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamid="
+const PROFILEURL = " https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=48CFA19B3E11A152DC3DAFE8CD6E1EF4&steamids="
 const FRIENDKEY = "&relationship=friend";
 const RECENTKEY = "&format=json"
 
